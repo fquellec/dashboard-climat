e22c0e09839ff5c23bd083643f5261b7cfcaa7e3
francoisquellec <francois.quellec@gmail.com>
2021-03-22 09:22:37 +0100

    fix map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 // Define Default constants
 const config = {
   width               : window.innerWidth,
-  height              : 600,
-  padding             : 100,
+  height              : 400,
+  padding             : 0,
   colorScaleDomain    : [0, 100],
   colorScaleRange     : ["#ececec", "#4285F4"],
   borderColor         : ["white", "grey"],
@@ -45,19 +45,26 @@ const tooltip = d3.select("#map")
     .style("pointer-events", "none")
     .style("box-shadow", "0.5px 0.8px 1px 0.5px");
 
+const zoom = d3.zoom().on("zoom", zoomed);
+var active = d3.select(null);
+
+const g =  map.append("g");
+var path;
+
 // Color scales
 //const colorScale = d3.scaleLinear()
 //  .domain(config.colorScaleDomain)
 //  .range(config.colorScaleRange);
 
-d3.json("assets/cantons_ch.json", function(error, geojson) {
-
+d3.json("assets/countries.json", function(error, geojson) {
+//Promise.all([d3.json("assets/cantons_chd.json")]).then(function(data) {
+  //const geojson = data[0];
   // Map projection to compute coordinates 
   const projection = d3.geoIdentity().reflectY(true).fitSize([config.width - config.padding*2, config.height - config.padding*2], geojson);
-  const path = d3.geoPath().projection(projection);
+  path = d3.geoPath().projection(projection);
 
   // Draw the map
-  map.append("g")
+  g
     .attr("transform", "translate(" + config.padding + "," + config.padding + ")")
     .selectAll("path")
       .data(geojson.features)
@@ -72,15 +79,56 @@ d3.json("assets/cantons_ch.json", function(error, geojson) {
         .style("stroke-width", "1px")
         .style("stroke-opacity", "1")
         .on('mouseover', function(event, d) {
-          d3.select(this).style('stroke', config.borderColor[1]).style("stroke-opacity", "1");
+          d3.select(this.parentNode.appendChild(this)).style('stroke', config.borderColor[1]).style("stroke-opacity", "1");
         }).on('mouseout', function(event, d) {
           d3.select(this).style('stroke', config.borderColor[0]).style("stroke-opacity", "1");
-        })
-      .exit()
-        .remove();
+        }).on("click", clicked);
 });
 
 
+function clicked(d) {
+  
+  if (active.node() === this) return reset();
+  active.classed("active", false);
+  active = d3.select(this).classed("active", true);
+
+  var bounds = path.bounds(d),
+      dx = bounds[1][0] - bounds[0][0],
+      dy = bounds[1][1] - bounds[0][1],
+      x = (bounds[0][0] + bounds[1][0]) / 2,
+      y = (bounds[0][1] + bounds[1][1]) / 2,
+      scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / config.width, dy / config.height))),
+      translate = [config.width / 2 - scale * x, config.height / 2 - scale * y];
+
+  svg.transition()
+      .duration(750)
+      // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+      .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+
+      
+}
+
+function zoomed() {
+  g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+  // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+  g.attr("transform", d3.event.transform); // updated for d3 v4
+}
+
+function reset() {
+  active.classed("active", false);
+  active = d3.select(null);
+
+  svg.transition()
+      .duration(750)
+      // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+      .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
+}
+
+// If the drag behavior prevents the default click,
+// also stop propagation so we don’t click-to-zoom.
+function stopped() {
+  if (d3.event.defaultPrevented) d3.event.stopPropagation();
+}
 
 
 
